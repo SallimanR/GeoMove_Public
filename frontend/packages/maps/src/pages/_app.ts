a89e9b3581f79945eb316ad580b1f6ee99724c9e
SallimanR <sallimanr888@gmail.com>
2026-06-28 21:43:56 +0000

    Remove: unnecessary comments

--- frontend/packages/maps/src/pages/_app.ts
+++ frontend/packages/maps/src/pages/_app.ts
@@ -1,9 +1,4 @@
 import { createApp } from 'vue';
 import type { App } from 'vue';
-// import { $drivers } from '@stores/drivers';
 
-// const app = createApp({});
-export default (app: App) => {
-	// app.use(devtools, { $drivers });
-	// app.use(clientRouter)
-}
+export default (app: App) => { }
