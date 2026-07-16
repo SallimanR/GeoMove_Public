166c2e15690acce5662a44d898686d5cbf92e8dd
SallimanR <sallimanr888@gmail.com>
2026-07-16 12:49:44 +0000

    Fix env variables for maps

--- frontend/packages/maps/src/env.d.ts
+++ frontend/packages/maps/src/env.d.ts
@@ -1,7 +1,6 @@
-// Astro expects "PUBLIC" prefix excplicitly defined
 interface ImportMetaEnv {
-	readonly PUBLIC_MAP_STYLE_API: string
-	readonly PUBLIC_MAP_TILES_AIP: string
+	readonly VITE_MAP_STYLE_API: string
+	readonly VITE_MAP_TILES_AIP: string
 }
 
 interface ImportMeta {
